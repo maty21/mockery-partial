e558979f68bbb7ec9846b00fa4c03f3481e90e92
maty21 <maty21@gmail.com>
2016-09-21 17:25:11 +0300

    first working

---
+++ test.js
@@ -0,0 +1,27 @@
+'use strict'
+
+var mockeryPartial =  require('./lib/mockery-partial');
+
+ //mocksipen.disable();
+
+var fsMock = {
+    stat: function (path, cb) { 
+        cb('yoopi'); 
+    }
+};
+mockeryPartial.enable();
+mockeryPartial.registerPartial('fs',fsMock);
+
+var fs = require('fs');
+fs.stat('a',(res)=>{
+ console.log(res)
+})
+let bla =fs.readFileSync('/home/matyz/node_modules/github_com_Hacker0x01_react-datepicker.pdf','utf8')
+
+ mockeryPartial.deregisterMock('fs');
+var fs2 = require('fs');
+
+fs2.stat('a',(res)=>{
+ console.log(res)
+})
+
